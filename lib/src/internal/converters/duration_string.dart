4c3462707e4b5a0a0ca5d551cc9b5cb0a61326d2
Jagandeep Brar <me@jagandeepbrar.io>
2023-02-13 20:43:43 -0500

    fix: handle all formats of C# TimeSpan 'c' format specifier

--- lib/src/internal/converters/duration_string.dart
+++ lib/src/internal/converters/duration_string.dart
@@ -5,23 +5,38 @@ class DurationStringConverter extends JsonConverter<Duration, String> {
 
   @override
   Duration fromJson(String json) {
-    int timeOffset = 0;
-    int days = 0;
-
-    final destructured = json.split('.');
-    if (destructured.length == 3) {
-      timeOffset = 1;
-      days = int.parse(destructured[0]);
-    }
-    final splitTime = destructured[timeOffset].split(':');
+    final destructured = _destructureJson(json);
+    final splitTime = destructured[1].split(':');
 
     return Duration(
-      days: days,
-      hours: int.parse(splitTime[0]),
-      minutes: int.parse(splitTime[1]),
-      seconds: int.parse(splitTime[2]),
-      microseconds: int.parse(destructured[timeOffset + 1]) ~/ 10,
+      days: int.parse(destructured[0]).abs(),
+      hours: int.parse(splitTime[0]).abs(),
+      minutes: int.parse(splitTime[1]).abs(),
+      seconds: int.parse(splitTime[2]).abs(),
+      microseconds: int.parse(destructured[2]).abs() ~/ 10,
     );
+  }
+
+  List<String> _destructureJson(String json) {
+    final destructured = json.split('.');
+
+    // ``hh:mm:ss``
+    if (destructured.length == 1) {
+      return ['0', json, '0'];
+    }
+
+    // `[d.]hh:mm:ss[.fffffff]`
+    if (destructured.length == 3) {
+      return destructured;
+    }
+
+    // `[d.]hh:mm:ss`
+    if (json.indexOf('.') < json.indexOf(':')) {
+      return [...destructured, '0'];
+    }
+
+    // `hh:mm:ss[.fffffff]`
+    return ['0', ...destructured];
   }
 
   @override
